f7b590efccf04c429670dbff10a6ed0adfa7d0e7
F-A-B-R-I-Z-I-O <fabriziofagiolo18@gmail.com>
2022-07-31 17:54:55 +0200

    redux-set-up

---
+++ MyUber/slices/navSlice.js
@@ -0,0 +1,46 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+/*
+    Inizialize App whit: 
+
+        Origin,
+        Destination,
+        Travel Time.
+    
+    And export to use in other component 
+
+*/
+
+const initialState = {
+  origin: null,
+  destinantion: null,
+  travelTimeInformation: null,
+};
+
+export const navSlice = createSlice({
+  name: "nav",
+  initialState,
+  reducer: {
+    setOrigin: (state, action) => {
+      state.origin = action.payload;
+    },
+    setDestination: (state, action) => {
+      state.destinantion = action.payload;
+    },
+    setTravelTimeInformation: (state, action) => {
+      state.travelTimeInformation = action.payload;
+    },
+  },
+});
+
+export const { setOrigin, setDestination, setTravelTimeInformation } =
+  navSlice.actions;
+
+// Selector
+
+export const selectOrigin = (state) => state.nav.origin;
+export const selectDestination = (state) => state.nav.destinantion;
+export const selectTravelTimeInformation = (state) =>
+  state.nav.travelTimeInformation;
+
+export default navSlice.reducer;
