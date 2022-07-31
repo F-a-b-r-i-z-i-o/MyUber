9cb14e20ee3b264461b97be0e3c53f4507c012b7
F-A-B-R-I-Z-I-O <fabriziofagiolo18@gmail.com>
2022-07-31 17:54:29 +0200

    redux-set-up

--- MyUber/App.js
+++ MyUber/App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Lets Build UBER!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Text>Lets Build UBER!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
   );
 }
 
