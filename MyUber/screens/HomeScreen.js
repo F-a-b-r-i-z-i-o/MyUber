8cdbdb10249e752146191a3bb3d1311794f41608
F-A-B-R-I-Z-I-O <fabriziofagiolo18@gmail.com>
2022-08-02 21:33:45 +0200

    add-uber-image

--- MyUber/screens/HomeScreen.js
+++ MyUber/screens/HomeScreen.js
@@ -1,12 +1,22 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
-import { SafeAreaView } from "react-native-web";
+import { StyleSheet, View, SafeAreaView, Image } from "react-native";
 import tw from "tailwind-react-native-classnames";
 
 const HomeScreen = () => {
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
-      <View></View>
+      <View>
+        <Image
+          style={{
+            width: 100,
+            height: 100,
+            resizeMode: "contain",
+          }}
+          source={{
+            uri: "https://links.papareact.com/gzs",
+          }}
+        />
+      </View>
     </SafeAreaView>
   );
 };
